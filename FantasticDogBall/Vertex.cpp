f4d614533c1d8b52b5ee5b8f3dd78d1df49b663d
Stefan <stefan.steinheber@gmail.com>
2022-03-29 17:23:26 +0200

    Added new libraries glew, glut and glm

---
+++ FantasticDogBall/Vertex.cpp
@@ -0,0 +1,8 @@
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+struct MeshVertex {
+	glm::vec3 position;
+	glm::vec3 normal;
+	glm::vec2 texture;
+};
--- FantasticDogBall/main.cpp
+++ FantasticDogBall/main.cpp
@@ -1,4 +1,7 @@
 #include <GLFW/glfw3.h>
+#include <GL/glew.h>
+#include <GL/glut.h>
+#include <cstdio>
 
 int main(void)
 {
@@ -8,6 +11,15 @@ int main(void)
     if (!glfwInit())
         return -1;
 
+    glutInit(&argc, argv);
+    glutCreateWindow("GLEW Test");
+    GLenum err = glewInit();
+    if (GLEW_OK != err)
+    {
+        /* Problem: glewInit failed, something is seriously wrong. */
+        fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+    }
+    fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
